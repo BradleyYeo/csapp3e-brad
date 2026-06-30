4a03d1e36483196e73bf15a3cc90b6c84984bc7b
Bradley Yeo <yeo.bradley@gmail.com>
2026-06-30 19:58:59 +0800

    remove unused files

--- bomb/hello.c
+++
@@ -1,7 +0,0 @@
-#include "stdio.h"
-
-int main(void) {
-  int x = 30000000000;
-  printf("%zu", sizeof(x));
-  printf("%d", x);
-}
--- bomb/test-struct.c
+++
@@ -1,6 +0,0 @@
-struct Unpacked {
-  char a;   // 1 byte
-  double b; // 8 bytes
-  int c;    // 4 bytes
-};
-struct Unpacked my_struct;
